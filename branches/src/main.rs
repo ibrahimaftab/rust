3fcdb4ebb9ddb737262aff9ab7a2a1fe63ddb458
Ibrahim Aftab <ibrahim.aftab@tapmad.com>
2025-03-07 15:51:21 +0500

    update .gitignore to exclude target directory and add Cargo.lock for branches package; enhance main function with conditional logic demonstration

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    let number = 5;
+
+    // if number < 5 {
+    //     println!("The condition was true");
+    // } else if number < 10 {
+    //     println!("The number is less than 10");
+    // } else {
+    //     println!("The condition was false");
+    // }
+
+    // Shorthand
+    let boolean = if number < 5 {
+        true
+    } else if number < 10 {
+        false
+    } else {
+        false
+    };
+
+    println!("The value of boolean is: {boolean}");
 }
