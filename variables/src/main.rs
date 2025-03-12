637906c349c0916b04984b6f1b7b690af97a49fd
Ibrahim Aftab <ibrahim.aftab@tapmad.com>
2025-03-12 11:56:02 +0500

    initialize functions package with Cargo.toml and Cargo.lock; implement main function demonstrating variable references and mutable strings

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -4,6 +4,10 @@ fn main() {
     x = 6;
     println!("The value of x is: {x}");
 
+    let y = &mut x;
+
+    println!("The value of y is: {y}");
+
     let test = "demo";
     println!("The value of test is: {test}");
     {
@@ -42,4 +46,22 @@ fn main() {
     println!("The value of c is: {c}");
     println!("The value of d is: {d}");
     println!("The value of d is: {e}");
+
+    let mut string = String::from("Hello");
+    println!("The value of string is: {string}");
+    let string2 = &mut string;
+    string2.push_str(" world");
+    // println!("The value of string is: {string}");
+    println!("The value of string2 is: {string2}");
+
+    let mut string3 = String::from("Happy");
+
+    let output = change(&mut string3);
+
+    println!("{}", output);
+}
+
+fn change(s: &mut String) -> &String {
+    s.push_str(" Folks");
+    s
 }
